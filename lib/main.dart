d96f895f314872eec860680be1c2395bed58376e
Parth Panchal <p.panchal9988@gmail.com>
2026-05-18 15:30:24 +0530

    splash screen completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_2/widgets/SplashScreen.dart';
 
 void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
         backgroundColor: const Color.fromARGB(255, 255, 252, 238),
-        
-        appBar: AppBar(
-          backgroundColor: const Color.fromARGB(255, 220, 155, 155),
-          foregroundColor: Colors.white,
-          title: Text(
-            "AppBar",
-            style: TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
-              fontStyle: FontStyle.italic,
-            ),
-          ),
-          leading: Icon(Icons.menu),
-          elevation: 4.0,
-          shadowColor: Color.fromARGB(0, 117, 117, 238),
-        ),
+        body: SplashScreen(),
+      
       ),
     ),
   );
