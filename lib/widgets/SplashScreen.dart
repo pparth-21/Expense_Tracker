40d64327ed7f3e801cb39f3008b5d38c2eeed682
amanpandav <amanpandav1222@gmail.com>
2026-05-19 13:46:12 +0530

    listing and splash working in sync
    main version 2

--- lib/widgets/SplashScreen.dart
+++ lib/widgets/SplashScreen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app_2/widgets/FrontPage.dart';
+import 'package:flutter_app_2/main.dart'; // ← change this import
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -10,15 +10,18 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
-    void initState() {
+  void initState() {
     super.initState();
-    Future.delayed(Duration(seconds: 3), () {        
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => FrontPage()), 
-      );
+    Future.delayed(Duration(seconds: 3), () {
+      if (mounted) {                              // ← add mounted check
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => FrontPageLayout()), // ← change this
+        );
+      }
     });
   }
+
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
@@ -34,7 +37,6 @@ class _SplashScreenState extends State<SplashScreen> {
       child: Column(
         children: [
           Padding(
-            
             padding: EdgeInsets.only(top: 8, right: 8),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.end,
@@ -42,43 +44,35 @@ class _SplashScreenState extends State<SplashScreen> {
             ),
           ),
           Expanded(
-            
-           
             child: Container(
-              margin: EdgeInsets.symmetric(horizontal:20 ,vertical: 200),
-              // padding: EdgeInsets.all(80),
+              margin: EdgeInsets.symmetric(horizontal: 20, vertical: 200),
               child: Column(
-              // mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              
-              children: [
-                Icon(Icons.currency_bitcoin_outlined, size: 100),
-                Text(
-                  "Spendly",
-                  style: TextStyle(
-                    fontSize: 40,
-                    fontStyle: FontStyle.italic,
-                    fontWeight: FontWeight.bold,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Icon(Icons.currency_bitcoin_outlined, size: 100),
+                  Text(
+                    "Spendly",
+                    style: TextStyle(
+                      fontSize: 40,
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                Text(
-                  '"WE HELP YOU SPEND WISELY!!:"',
-                  style: TextStyle(fontSize: 15, ),
-                ),
-                SizedBox(height: 150),
-                CircularProgressIndicator(
-                  color: Color.fromARGB(110, 54, 54, 54),
-                ),
-                SizedBox(height: 20),
-                Text("Loading..."),
-                
-              
-              ],
+                  Text(
+                    '"WE HELP YOU SPEND WISELY!!:"',
+                    style: TextStyle(fontSize: 15),
+                  ),
+                  SizedBox(height: 150),
+                  CircularProgressIndicator(
+                    color: Color.fromARGB(110, 54, 54, 54),
+                  ),
+                  SizedBox(height: 20),
+                  Text("Loading..."),
+                ],
+              ),
             ),
           ),
-          ),
           Padding(
-            
             padding: EdgeInsets.only(bottom: 16),
             child: Text("Copyright © 2026 Spendly. All rights reserved."),
           ),
